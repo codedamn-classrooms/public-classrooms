d293706b091f755dd18bea5ecc47a33cd7a25875
Mehul Mohan <mehulmpt@gmail.com>
2021-01-31 23:58:29 +0530

    repo ready

---
+++ index.js
@@ -0,0 +1,14 @@
+// Initialize express server on PORT 1337
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+// create a new route for "/hello-world"
+
+app.listen(process.env.PUBLIC_PORT, () => {
+	console.log(`Server started and listening on port ${process.env.PUBLIC_PORT}`)
+})
